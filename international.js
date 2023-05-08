f1e4255db06f4278634277aa6c3a7968ef623c77
nat-0813 <129187214+nat-0813@users.noreply.github.com>
2023-05-08 16:40:44 -0400

    commit-all pages

--- international.js
+++ international.js
@@ -32,3 +32,35 @@ procontainer.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+//Products JS//
+
+// Fetch the JSON file
+fetch('products.json')
+.then(response => response.json())
+.then(products => {
+  // Get the product list container
+  const productList = document.getElementById('product-list');
+  // Loop through the products and create list items for each one
+  products.forEach(product => {
+    const listItem = document.createElement('li');
+    const image = document.createElement('img');
+    image.src = product.image;
+    const title = document.createElement('h2');
+    title.textContent = product.title;
+    const description = document.createElement('h4');
+    description.textContent = product.description;
+    const price = document.createElement('p');
+    price.textContent = `$${product.price.toFixed(2)}`;
+    // Append the product information to the list item
+    listItem.appendChild(image);
+    listItem.appendChild(title);
+    listItem.appendChild(description);
+    listItem.appendChild(price);
+    // Append the list item to the product list container
+    productList.appendChild(listItem);
+  });
+})
+.catch(error => {
+  console.error('Error fetching products:', error);
+});
